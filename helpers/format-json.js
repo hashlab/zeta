02886534cfefc8e77a8e4d1cdfda513394dac391
Christopher Enytc <chris@enytc.com>
2018-01-13 15:29:55 -0200

    Update format-json helper

--- helpers/format-json.js
+++ helpers/format-json.js
@@ -1,5 +1,9 @@
 const PrettyJSON = require("prettyjson");
 
-module.exports = function formatJSON(body) {
+module.exports = function formatJSON(body, disableMarkdown = false) {
+  if (disableMarkdown) {
+    return PrettyJSON.render(body);
+  }
+
   return `\`\`\`\n${PrettyJSON.render(body)}\n\`\`\``;
 };
