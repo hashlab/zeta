77e40d50efe53ace57c20cf89b912ac70742a1c2
Christopher Enytc <chris@enytc.com>
2018-01-15 08:14:40 -0200

    Remove unused method on aws s3 script

--- scripts/aws-s3.js
+++ scripts/aws-s3.js
@@ -15,7 +15,6 @@
 // Commands:
 //   hubot s3 buckets - Get a list of all buckets on AWS S3
 //   hubot s3 create bucket <bucket-name> <is-private> - Create a new bucket on AWS S3
-//   hubot s3 bucket <bucket-name> - Get details of a bucket website on AWS S3
 //   hubot s3 enable website for bucket <bucket-name> - Enable static website mode for a bucket on AWS S3
 //   hubot s3 set policy for bucket <bucket-name> - Set website policy for a bucket on AWS S3
 //   hubot s3 get url for bucket <bucket-name> - Get the url of a bucket website on AWS S3
@@ -164,58 +163,6 @@ module.exports = function S3Script(robot) {
         "Bucket created successfully.",
         "success"
       );
-    }
-
-    function respond(response) {
-      return res.send(FormatJSON(response));
-    }
-
-    return S3Promise;
-  });
-
-  robot.respond(/s3 bucket (.*)/i, res => {
-    if (!CheckEnv(robot, "HUBOT_AWS_REGION")) {
-      return null;
-    }
-
-    if (!CheckEnv(robot, "HUBOT_AWS_ACCESS_KEY_ID")) {
-      return null;
-    }
-
-    if (!CheckEnv(robot, "HUBOT_AWS_SECRET_ACCESS_KEY")) {
-      return null;
-    }
-
-    const BucketName = R.replace(/http:\/\//g, "", res.match[1]);
-
-    const S3Promise = Promise.resolve()
-      .tap(checkUserPermission)
-      .then(getBucketWebsite)
-      .then(respond)
-      .catch(ErrorHandler(robot, res, "s3 bucket <bucket-name>"));
-
-    function checkUserPermission() {
-      return Promise.resolve()
-        .then(CheckPermission(robot, res))
-        .tap(hasPermission => {
-          if (!hasPermission) {
-            return S3Promise.cancel();
-          }
-          return null;
-        });
-    }
-
-    function getBucketWebsite() {
-      // eslint-disable-next-line promise/avoid-new
-      return new Promise((resolve, reject) => {
-        S3Client.getBucketWebsite({ Bucket: BucketName }, function(err, data) {
-          if (err) {
-            return reject(err);
-          }
-
-          return resolve(data);
-        });
-      });
     }
 
     function respond(response) {
